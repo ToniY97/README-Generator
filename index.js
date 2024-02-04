a2c826a24605d1ae0e1f4ff4fc1a919665675f2c
Toni97 <ayovcheva75@gmail.com>
2024-02-04 16:30:26 +0000

    added questions for user

--- index.js
+++ index.js
@@ -5,6 +5,52 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a short description of your project:'
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'How do you install your project?'
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use your project?'
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', 'None']
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'How can others contribute to your project?'
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide any tests for your project and how to run them:'
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?'
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+      }
 
 ];
 
