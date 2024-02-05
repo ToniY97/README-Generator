6a087fbb2d0b7e11c8f53dd0fbf0e6b35be761dc
Toni97 <ayovcheva75@gmail.com>
2024-02-05 19:03:07 +0000

    final code and generated README

---
+++ generated_README/README.md
@@ -0,0 +1,33 @@
+# README Generator
+
+## Description
+a js code that generate a readme file
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+type node index.js
+
+## Usage
+I use it to generate readme files for each project
+
+## License
+This project is licensed under the MIT license.
+![License](https://img.shields.io/badge/license-MIT-green)
+
+## Contributing
+NA
+
+## Tests
+NA
+
+## Questions
+If you have any questions, feel free to reach out:
+- GitHub: [random.github](https://github.com/random.github)
+- Email: random.email
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,37 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+This project is licensed under the ${data.license} license.
+![License](https://img.shields.io/badge/license-${encodeURIComponent(data.license)}-green)
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+If you have any questions, feel free to reach out:
+- GitHub: [${data.github}](https://github.com/${data.github})
+- Email: ${data.email}
 `;
 }
 
